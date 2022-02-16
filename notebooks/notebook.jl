227e0c3d8fa864c6ef8409ef5a494481e07a0fa2
Ian Weaver <iweaver@mailbox.org>
2022-02-16 10:50:58 -0500

    Db.jl up

---
+++ notebooks/notebook.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 47d8184c-8f3f-11ec-2252-b78e9946c30a
+begin
+	import Pkg
+	Pkg.activate(Base.current_project())
+	import Db
+end
+
+# ╔═╡ 184bc985-510c-4961-82bf-c975a73eca41
+Db.my_db_function()
+
+# ╔═╡ Cell order:
+# ╠═47d8184c-8f3f-11ec-2252-b78e9946c30a
+# ╠═184bc985-510c-4961-82bf-c975a73eca41
--- src/Db.jl
+++ src/Db.jl
@@ -2,4 +2,8 @@ module Db
 
 # Write your package code here.
 
+export my_db_function
+
+my_db_function() = "I'm a function inside src/Db.jl!"
+
 end
